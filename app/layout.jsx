21104757aa2339b4a08dd56e1a12fb855288f8a2
bmcmillybooking-cloud <bmcmillybooking@gmail.com>
2026-01-21 16:32:52 -0600

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,10 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "BMC Milly",
+  description: "Underrated but never overlooked",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
