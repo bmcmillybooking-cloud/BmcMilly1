211374a0dc26ae22d7655af8d0c6604beca3e257
bmcmillybooking-cloud <bmcmillybooking@gmail.com>
2026-01-21 16:13:27 -0600

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,4 @@
-export const metadata = {
-  title: "BMC Milly",
-  description: "Official BMC Milly site",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
