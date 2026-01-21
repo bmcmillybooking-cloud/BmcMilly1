f577c7317703f77ce85aadc3f0b77a66c1013f8f
bmcmillybooking-cloud <bmcmillybooking@gmail.com>
2026-01-21 14:47:51 -0600

    Add Homepage

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>BMC Milly</h1>
+      <p>Your site is live 🚀</p>
+    </main>
+  );
+}
